d0480fd3ef50d543b0b2951a81ec906ea63f8930
karimamnr <karimamnr130@gmail.com>
2025-12-06 16:54:49 +0100

    fix background

--- src/LandingHome.jsx
+++ src/LandingHome.jsx
@@ -40,22 +40,22 @@ export default function LandingPage() {
         <div
           className="absolute inset-0"
           style={{
-            backgroundImage: `linear-gradient(#669BBC 1px, transparent 1px), linear-gradient(90deg, #669BBC 1px, transparent 1px)`,
+            // backgroundImage: `linear-gradient(#669BBC 1px, transparent 1px), linear-gradient(90deg, #669BBC 1px, transparent 1px)`,
             backgroundSize: "50px 50px",
           }}
         ></div>
       </div>
 
       <div className="fixed inset-0 pointer-events-none">
-        {[...Array(20)].map((_, i) => (
+        {[...Array(90)].map((_, i) => (
           <div
             key={i}
             className="absolute w-1 h-1 bg-[#A5CEE7] rounded-full animate-pulse"
             style={{
               left: `${Math.random() * 100}%`,
               top: `${Math.random() * 100}%`,
-              animationDelay: `${Math.random() * 3}s`,
-              animationDuration: `${2 + Math.random() * 3}s`,
+              animationDelay: `${Math.random() * 7}s`,
+              animationDuration: `${20 + Math.random() * 7}s`,
             }}
           ></div>
         ))}
