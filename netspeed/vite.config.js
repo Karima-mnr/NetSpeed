1383953ad6394dcae0d84530056a1a989802d2f2
karimamnr <karimamnr130@gmail.com>
2025-12-06 14:03:22 +0100

    change it vite.config

--- netspeed/vite.config.js
+++ netspeed/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/', // ensures all assets are served from the root
   plugins: [react()],
+  base: './', // important for Vercel, relative paths
 })
