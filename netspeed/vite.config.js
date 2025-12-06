d2d996eb864f87ec59a6853b39910071e3dad61d
karimamnr <karimamnr130@gmail.com>
2025-12-06 13:18:43 +0100

    Fix Vite base and Vercel rewrites

--- netspeed/vite.config.js
+++ netspeed/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // ensures all assets are served from the root
   plugins: [react()],
 })
